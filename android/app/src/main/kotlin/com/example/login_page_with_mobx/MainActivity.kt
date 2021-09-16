03a52e9d3d2d89db1e901e2fc5dae53262b2bc5a
Gabriel Kohler <kohler2018@outlook.com>
2021-09-15 21:35:16 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/login_page_with_mobx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.login_page_with_mobx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
