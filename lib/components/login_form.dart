4840001baa609c67ccc9f690852a71a156a3ef3a
Gabriel Kohler <kohler2018@outlook.com>
2021-09-16 10:46:41 -0300

    fix(view): bug fix in login form

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -1,62 +1,66 @@
 import 'package:flutter/material.dart';
 
 class LoginForm extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //const SizedBox(height: 100),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 28, vertical: 10),
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.person,
-                color: const Color(0xFFE87653),
-              ),
-              labelText: 'Email',
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: BorderSide(
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 28, vertical: 10),
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.person,
                   color: const Color(0xFFE87653),
+                ),
+                labelText: 'Email',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(
+                    color: const Color(0xFFE87653),
+                  ),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 28, vertical: 5),
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: const Color(0xFFE87653),
+                ),
+                labelText: 'Password',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(
+                    color: const Color(0xFFE87653),
+                  ),
                 ),
               ),
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
           ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 28, vertical: 5),
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-          child: TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.lock,
-                color: const Color(0xFFE87653),
-              ),
-              labelText: 'Password',
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: BorderSide(
-                  color: const Color(0xFFE87653),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -2,27 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:login_page_with_mobx/components/login_form.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final screenHeight = (size.height - MediaQuery.of(context).padding.top);
+    //final screenHeight = (size.height - MediaQuery.of(context).padding.top);
     return Scaffold(
       //appBar: AppBar(),
       body: Container(
-        color: Colors.white,
-        height: size.height,
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: screenHeight * 0.2),
+              SizedBox(height: 150),
               Container(
-                height: screenHeight * 0.43,
+                height: 350,
                 child: Image.asset('assets/img/imglogin.png'),
               ),
               Container(
-                height: screenHeight * 0.37,
+                height: 250,
                 child: LoginForm(),
               ),
             ],
