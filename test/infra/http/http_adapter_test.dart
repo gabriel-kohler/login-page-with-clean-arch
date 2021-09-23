4a663a2621301c043da279a1d249e232aea3c6a1
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-09-23 10:55:21 -0300

    test(infra): ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:faker/faker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -18,7 +19,7 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(Uri.parse(url), headers: headers);
+    await client.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -35,14 +36,18 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
       final uriUrl = Uri.parse(url);
 
-      verify(client.post(uriUrl, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json',
-      }));
+      verify(client.post(
+        uriUrl,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+        body: jsonEncode({"any_key": "any_value"}),
+      ));
     });
   });
 }
