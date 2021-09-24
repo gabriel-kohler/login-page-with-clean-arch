81586a35268d809791ec5676ab5834772009b7ec
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-09-24 16:52:47 -0300

    move duplicated code to the setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -40,9 +40,15 @@ void main() {
     url = faker.internet.httpUrl();
   });
   group('post', () {
-    test('Should call post with correct values', () async {
 
-      when(client.post(any, headers: anyNamed('headers'), body: anyNamed('body'))).thenAnswer((_) async => Response('{"any_key": "any_value"}', 200));
+    PostExpectation mockRequest() => when(client.post(any, headers: anyNamed('headers'), body: anyNamed('body')));
+
+    mockResponse(int statusCode, {String body = '{"any_key": "any_value"}'}) => mockRequest().thenAnswer((_) async => Response(body, statusCode));
+
+    setUp(() {
+      mockResponse(200);
+    });
+    test('Should call post with correct values', () async {
 
       await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
@@ -60,8 +66,6 @@ void main() {
 
     test('Should call post without body', () async {
 
-      when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('{"any_key": "any_value"}', 200));
-
       await sut.request(url: url, method: 'post');
 
       verify(client.post(
@@ -72,8 +76,6 @@ void main() {
 
     test('Should return data if post returns 200', () async {
 
-      when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('{"any_key": "any_value"}', 200));
-
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
@@ -82,7 +84,7 @@ void main() {
 
     test('Should return null if post returns 200 without data', () async {
 
-      when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('', 200));
+      mockResponse(200, body: '');
 
       final response = await sut.request(url: url, method: 'post');
 
