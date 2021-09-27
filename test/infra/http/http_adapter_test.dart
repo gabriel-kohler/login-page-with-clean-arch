c40c1099444fc9eb9d81ef7067ba0a6fe1d3d2e2
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-09-27 18:55:49 -0300

    refactor(infra): move HttpAdapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,36 +1,12 @@
 import 'dart:convert';
 import 'package:faker/faker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:login_page_with_mobx/data/http/http.dart';
+import 'package:login_page_with_mobx/infra/http/http.dart';
 import 'package:mockito/mockito.dart';
 
-class HttpAdapter implements HttpClient {
-  final Client client;
 
-  HttpAdapter(this.client);
-
-  @override
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-
-    if (response.statusCode == 200) {
-      return response.body.isNotEmpty ? jsonDecode(response.body) : null;
-    } else {
-      return null;
-    }
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
@@ -113,6 +89,16 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
 
     });
 
