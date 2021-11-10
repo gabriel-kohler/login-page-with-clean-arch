c4e84ae515db70e89c803f04cc04d51452630366
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-11-10 14:46:07 -0300

    test(presentation): ensure call Validation with correct values

---
+++ test/presentation/stream_login_presenter_test.dart
@@ -0,0 +1,37 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+abstract class Validation {
+  String validate({@required String field, @required String value});
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+class StreamLoginPresenter {
+
+  final Validation validation;
+
+  StreamLoginPresenter({@required this.validation});
+
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
+
+  
+}
+
+void main() {
+  test('Should call Validation with correct values', () {
+    final validation = ValidationSpy();
+    final sut = StreamLoginPresenter(validation: validation);
+
+    final email = faker.internet.email();
+
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+    
+  });
+}
