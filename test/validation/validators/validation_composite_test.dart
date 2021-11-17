52d5f2ea19227625467cc31bf7fa27c47b371bb0
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-11-17 14:04:16 -0300

    test(validation): ensure ValidationComposite return null if all validations returns null or empty

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -1,7 +1,6 @@
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:login_page_with_mobx/validation/validators/validators.dart';
 import 'package:login_page_with_mobx/validation/dependencies/dependencies.dart';
 
 import 'package:login_page_with_mobx/presentation/dependencies/dependencies.dart';
@@ -25,16 +24,38 @@ class ValidationComposite implements Validation {
 
 void main() {
 
+  FieldValidationSpy validation1;
+  FieldValidationSpy validation2;
+  List<FieldValidation> validations;
+  ValidationComposite sut;
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    validation2 = FieldValidationSpy();
+    validations = [validation1, validation2];
+    sut = ValidationComposite(validations: validations);
+
+    when(validation1.field).thenReturn('any_field');
+    when(validation2.field).thenReturn('any_field');
+  });
+
   test('Should call validate with correct values', () {
-
-    final FieldValidationSpy validation1 = FieldValidationSpy();
-    final FieldValidationSpy validation2 = FieldValidationSpy();
-    final List<FieldValidation> validations = [validation1, validation2];
-
-    final sut = ValidationComposite(validations: validations);
 
     sut.validate(field: 'any_field', value: 'any_value');
 
     verify(validation1.validate(value: 'any_value')).called(1);
   });
+
+  test('Should return null if all validations returns null', () {
+    when(validation1.validate(value: anyNamed('value'))).thenReturn(null);
+    when(validation1.validate(value: anyNamed('value'))).thenReturn('');
+
+    when(validation2.validate(value: anyNamed('value'))).thenReturn(null);
+    when(validation2.validate(value: anyNamed('value'))).thenReturn('');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, null);
+  });
+
 }
