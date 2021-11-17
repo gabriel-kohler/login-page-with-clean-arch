bf798c5a86b70953f700d29ee320f86d824becc0
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-11-17 14:54:05 -0300

    test(validation): ensure ValidationComposite return error if validation return error

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -15,8 +15,16 @@ class ValidationComposite implements Validation {
 
   @override
   String validate({String field, String value}) {
-    validations.first.validate(value: value);
-    return null;
+    String error;
+    for (final validation in validations) {
+      error = validation.validate(value: value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      } else {
+        return null;
+      }
+    }
+    return error;
   }
 
 }
@@ -30,23 +38,26 @@ void main() {
   ValidationComposite sut;
 
   void mockValidation1(String error) => when(validation1.validate(value: anyNamed('value'))).thenReturn(error);
-  void mockValidation2(String error) => when(validation1.validate(value: anyNamed('value'))).thenReturn(error);
+  void mockValidation2(String error) => when(validation2.validate(value: anyNamed('value'))).thenReturn(error);
+
 
   setUp(() {
     validation1 = FieldValidationSpy();
     validation2 = FieldValidationSpy();
     validations = [validation1, validation2];
-    sut = ValidationComposite(validations: validations);
 
-    when(validation1.field).thenReturn('any_field');
+    when(validation1.field).thenReturn('other_field');
     mockValidation1(null);
     when(validation2.field).thenReturn('any_field');
     mockValidation2(null);
+
+    sut = ValidationComposite(validations: validations);
+
   });
 
   test('Should call validate with correct values', () {
 
-    sut.validate(field: 'any_field', value: 'any_value');
+    sut.validate(field: 'other_field', value: 'any_value');
 
     verify(validation1.validate(value: 'any_value')).called(1);
   });
@@ -58,6 +69,15 @@ void main() {
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
+  });
+
+  test('Should return first error if validation return error', () {
+    mockValidation1('error1');
+    mockValidation2('error2');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error1');
   });
 
 }
