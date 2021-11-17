d0ed5561794f89b01ffd4bd9d1503b5621c45fbf
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-11-17 01:32:21 -0300

    test(validation): ensure RequiredFieldValidation return error if value is null

---
+++ test/validation/validators/required_field_validation_test.dart
@@ -0,0 +1,38 @@
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+abstract class FieldValidation {
+  String get field;
+  String validate({@required String value});
+}
+
+class RequiredFieldValidation implements FieldValidation {
+  final String field;
+
+  RequiredFieldValidation(this.field);
+
+  @override
+  String validate({String value}) {
+    return 'Campo obrigatório';
+  }
+
+
+}
+
+void main() {
+
+  RequiredFieldValidation sut;
+
+  setUp(() {
+    sut = RequiredFieldValidation('any_field');
+  });
+
+  test('Should return error if value is null', () {
+
+    final error = sut.validate(value: null);
+
+    expect(error, 'Campo obrigatório');
+
+  });
+
+}
