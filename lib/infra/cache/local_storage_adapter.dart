6720f3bd65919344c89ea462fdee94c5259f870f
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-11-24 11:01:34 -0300

    feat(infra): move production code to its own file

---
+++ lib/infra/cache/local_storage_adapter.dart
@@ -0,0 +1,16 @@
+import '/data/cache/cache.dart';
+import 'package:meta/meta.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+
+  final FlutterSecureStorage secureStorage;
+
+  LocalStorageAdapter({@required this.secureStorage});
+
+  @override
+  Future<void> saveSecure({@required String key,@required String value}) async {
+      await secureStorage.write(key: key, value: value);     
+  }
+}
--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,22 +1,9 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:login_page_with_mobx/data/cache/cache.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
-
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-
-  final FlutterSecureStorage secureStorage;
-
-  LocalStorageAdapter({@required this.secureStorage});
-
-  @override
-  Future<void> saveSecure({@required String key,@required String value}) async {
-      await secureStorage.write(key: key, value: value);     
-  }
-}
+import 'package:login_page_with_mobx/infra/cache/cache.dart';
 
 class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
