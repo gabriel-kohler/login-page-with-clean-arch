e5d1865ddccdf3133ee3b8f0ae911a110ebf8a4a
Gabriel Kohler <85637999+gabriel-kohler@users.noreply.github.com>
2021-12-01 00:14:55 -0300

    test(presentation): ensure GetxSplashPresenter go to HomePage on success

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
+
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
 import 'package:login_page_with_mobx/ui/pages/pages.dart';
 import 'package:login_page_with_mobx/domain/usecases/load_current_account.dart';
 import 'package:login_page_with_mobx/data/usecases/load_current_account/load_current_account.dart';
+import 'package:login_page_with_mobx/domain/entities/account_entity.dart';
+import 'package:login_page_with_mobx/utils/app_routes.dart';
 
 class LocalLoadCurrentAccountSpy extends Mock implements LocalLoadCurrentAccount {}
 
@@ -19,7 +22,14 @@ class GetxSplashPresenter implements SplashPresenter {
 
   @override
   Future<void> checkAccount() async {
-    await loadCurrentAccount.load();
+    final account = await loadCurrentAccount.load();
+    
+    if (account != null) {
+      if (account.token?.isNotEmpty == true) {
+      _navigateTo.value = AppRoutes.HOME_PAGE;
+      }
+    }
+    
   }
 
   @override
@@ -42,5 +52,21 @@ void main() {
     await sut.checkAccount();
 
     verify(loadCurrentAccountSpy.load()).called(1);
+  });
+
+  test('Should go to home page on success', () async {
+
+    when(loadCurrentAccountSpy.load()).thenAnswer((_) async => AccountEntity('any_value'));
+
+    sut.navigateToStream.listen(
+      expectAsync1(
+        (page) {
+        expect(page, AppRoutes.HOME_PAGE);
+        }
+      ),
+    );
+
+    await sut.checkAccount();
+
   });
 }
